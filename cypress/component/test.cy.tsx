345befd3b1a4cc864ab18ecfb377cfe8b4b36b35
SERHIIBZENKD8F4\serhiibzenko <bzenkosergey@gmail.com>
2022-08-20 20:29:02 +0200

    fix if generation

--- cypress/component/test.cy.tsx
+++ cypress/component/test.cy.tsx
@@ -4,15 +4,13 @@ import { mount } from '@cypress/react'
 import { activeTab, cssClasses, localization, disabled, tabChange, tabs, hideTabs, values } from '@sbzen/e2e';
 import { App } from './app';
 
-describe('E2e tests', () => {
-  beforeEach(() => mount(<App/>));
+const beforeEachCb = () => beforeEach(() => mount(<App/>));
 
-  activeTab();
-  cssClasses();
-  localization();
-  disabled();
-  tabChange();
-  tabs();
-  hideTabs();
-  values();
-});
+activeTab(beforeEachCb);
+cssClasses(beforeEachCb);
+localization(beforeEachCb);
+disabled(beforeEachCb);
+tabChange(beforeEachCb);
+tabs(beforeEachCb);
+hideTabs(beforeEachCb);
+values(beforeEachCb);
